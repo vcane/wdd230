96e4cc9270ede677b62e7d336fa32c23276e4561
vcane <vcane6@gmail.com>
2021-09-16 21:00:46 -0500

    final push for week 1 assignment

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//code below is from 9/15/21 WDD230 Virtual Lab
+const copyrightyr = document.getElementById('copyrightyr');
+copyrightyr.textContent = new Date().getFullYear();
+
+// found assistance with below code from https://tecadmin.net/get-current-date-time-javascript/
+const lastupdate = new Date();
+document.getElementById('lastupdate').innerHTML = `${lastupdate.getMonth()+1}/${lastupdate.getDate()}/${lastupdate.getFullYear()} 
+${lastupdate.getHours()}:${lastupdate.getMinutes()}:${lastupdate.getSeconds()}`;
