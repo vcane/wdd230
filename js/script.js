760b452f6f3d72d120c0e1fdf8d494e8890e90db
vcane <vcane6@gmail.com>
2021-09-20 19:10:23 -0500

    revisions after Bro Blazzard's review

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-//code below is from 9/15/21 WDD230 Virtual Lab
-const copyrightyr = document.getElementById('copyrightyr');
+///code below is from 9/15/21 WDD230 Virtual Lab
+c/onst copyrightyr = document.getElementById('copyrightyr');
 copyrightyr.textContent = new Date().getFullYear();
 
+//code below is from my first try at the course portal home page
 // found assistance with below code from https://tecadmin.net/get-current-date-time-javascript/
-const lastupdate = new Date();
-document.getElementById('lastupdate').textContent = `${lastupdate.getMonth()+1}/${lastupdate.getDate()}/${lastupdate.getFullYear()} 
-${lastupdate.getHours()}:${lastupdate.getMinutes()}:${lastupdate.getSeconds()}`;
+//const lastupdate = new Date();
+//document.getElementById('lastupdate').textContent = `${lastupdate.getMonth()+1}/${lastupdate.getDate()}/${lastupdate.getFullYear()} 
+//${lastupdate.getHours()}:${lastupdate.getMinutes()}:${lastupdate.getSeconds()}`;
+
+// revised code after Bro Blazzard's review on 9/20/21
+const lastupdate = document.lastModified;
+document.getElementById('lastupdate').textContent = lastupdate;
