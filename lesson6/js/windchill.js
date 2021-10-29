c9abc1bd2f2059c58ca324322f290dbdb46f2061
vcane <vcane6@gmail.com>
2021-10-28 19:32:59 -0500

    Weather gallery init commit push Course portal update weather gallery

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,10 @@
 //t = current temp in degree F;
 //s = wind speed in mph;
-//f = current windchill = 35.74 + 0.6215 * t - 35.75 * s^0.16 + 0.4275 * t * s^0.16, if t <= 50 && s >= 3;
+//f = current windchill = 35.74 + 0.6215 * t - 35.75 * s^0.16 + 0.4275 * t * s^0.16, if t <= 50 && s > 3;
 const t = parseFloat(document.querySelector('.t').textContent);
 const s = parseFloat(document.querySelector('.s').textContent);
 let f;
-if (t <= 50 && s >= 3) {
+if (t <= 50 && s > 3) {
   f = `${(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16))).toFixed(0)}°F`;
 }
 
--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,15 +1,46 @@
+// get all imgs with data-src attribute
 let imagesToLoad = document.querySelectorAll('[data-src]');
+
+// test with console.log(imagesToLoad);
+//optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 0.5
+  
+};
+
+// take image and set src attribute to data-src attribute value then onload remove data-src attribute
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => { 
     image.removeAttribute('data-src');
   };
 };
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
 
-if('IntersectionObserver' in window) {
+// first check to see if intersection observer is supported
+if ('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach(item => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  // loop through each img and check status and load if necessary
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+
+} else { // just load ALL images if not supported
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+
+/*if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver ((items, observer) => {
     items.forEach((item) => {
       if(item.isIntersecting) {
@@ -27,4 +58,4 @@ else {
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
-}
+}*/
