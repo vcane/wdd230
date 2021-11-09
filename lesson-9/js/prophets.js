5875213343fc213a08cd279ba03a106c13ff1fac
vcane <vcane6@gmail.com>
2021-11-08 18:31:48 -0600

    prophets push, tablebuild revision push

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    for (let i = 0; i < prophets.length; i++ ) {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let dob = document.createElement('p');
+      let pob = document.createElement('p');
+      let image = document.createElement('img');
+      
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      dob.textContent = prophets[i].birthdate;
+      pob.textContent = prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+
+      card.appendChild(h2);
+      card.appendChild(dob);
+      card.appendChild(pob);
+      card.appendChild(image);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+  });
+  
